63e02de3c70a0a5d1318e99a09cf806cf770056a
Mike Huwusky <Mike.huwusky@gmail.com>
2022-11-20 21:57:39 +0100

    Creation of "int divider" (09/03/2022)
    Check every divider that make the number an integer

---
+++ diviseur entier.py
@@ -0,0 +1,18 @@
+nmb = float(input("Entrez un nombre : "))
+liste = []
+listef = []
+
+for x in range(1, nmb):
+    nmb / x
+    print(type)
+
+if nmb.is_integer():
+    print(f'les diviseur entier de {int(nmb)} sont :')
+    for terms in range(len(liste)):
+        print(listef[terms])
+else:
+    print(f'les diviseur entier de {nmb} sont :')
+    for terms in range(len(liste)):
+        print(listef[terms])
+
+input('Appuyer ENTER pour quitter')
